a38473e703ab6dc62a3ec91d4d90ba0ce1eb5364
Kayden <kayden02302@gmail.com>
2022-11-27 20:11:14 +0700

    update 27-11

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Nghi Long'
+  const age = '19'
+
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Trixie
         </p>
+
+      <p>Hello {name} {age} chủi</p>
       </header>
     </div>
   );
